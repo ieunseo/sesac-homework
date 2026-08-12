e3053cf7f700ebf62416447d88cffd79a4979275
ieunseo <y949512@naver.com>
2026-08-12 23:55:13 +0900

    #1 - 페이지네이션추가

--- index-script.js
+++ index-script.js
@@ -11,6 +11,14 @@ let commentListData = [];
 /* 검색 타이머 */
 let searchTimer = "";
 
+/* 일기 개수 */
+const DIARY_COUNT_PER_PAGE = 12;
+/* 버튼 개수 */
+const PAGE_COUNT_PER_GROUP = 10;
+
+let startPage = 1;
+let currentPage = 1;
+
 /* HTML 요소 */
 const diaryForm = document.getElementById("diary-form");
 const diaryListArea = document.getElementById("diary-list-area");
@@ -41,6 +49,16 @@ const darkmode = document.querySelector(".dark-toggle");
 const formPanel = document.querySelector(".form-panel");
 const successModal = document.querySelector(".success-modal");
 
+/* 페이지네이션 */
+const pagination = document.getElementById("pagination");
+const paginationNumberArea = document.getElementById("pagination-number-area");
+
+const paginationPrevButton = document.getElementById("pagination-prev-button");
+const paginationNextButton = document.getElementById("pagination-next-button");
+
+const paginationPrevImage = document.getElementById("pagination-prev-image");
+const paginationNextImage = document.getElementById("pagination-next-image");
+
 /* =========================
    LocalStorage
 ========================= */
@@ -69,10 +87,7 @@ const loadData = () => {
             JSON.parse(savedData);
 
 
-        /*
-         * 기존에 저장했던 데이터가
-         * 일기 배열만 있는 형태일 경우
-         */
+        /* 기존에 저장했던 데이터가 일기 배열만 있는 형태일 경우*/
         if (Array.isArray(parsedData)) {
             diaryListData = parsedData;
             commentListData = [];
@@ -80,19 +95,9 @@ const loadData = () => {
             return;
         }
 
+        diaryListData = parsedData.diaryList || [];
 
-        /*
-         * 새로운 저장 형태
-         * {
-         *     diaryList: [],
-         *     commentList: []
-         * }
-         */
-        diaryListData =
-            parsedData.diaryList || [];
-
-        commentListData =
-            parsedData.commentList || [];
+        commentListData = parsedData.commentList || [];
 
     } catch {
 
@@ -111,9 +116,7 @@ const saveData = () => {
     };
 
 
-    localStorage.setItem(
-        STORAGE_KEY,
-        JSON.stringify(storageData)
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(storageData)
     );
 };
 
@@ -199,16 +202,11 @@ const getTodayText = () => {
 
     const today = new Date();
 
-    const year =
-        today.getFullYear();
+    const year = today.getFullYear();
 
-    const month = String(
-        today.getMonth() + 1
-    ).padStart(2, "0");
+    const month = String(today.getMonth() + 1).padStart(2, "0");
 
-    const day = String(
-        today.getDate()
-    ).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
 
     return `${year}. ${month}. ${day}`;
 };
@@ -217,10 +215,7 @@ const getTodayText = () => {
 /* 선택된 감정 라디오 버튼 값 가져오기 */
 const getSelectedMood = () => {
 
-    const moodInputs =
-        document.querySelectorAll(
-            'input[name="mood"]'
-        );
+    const moodInputs = document.querySelectorAll('input[name="mood"]');
 
     let selectedMood = "기타";
 
@@ -241,8 +236,7 @@ const getSelectedMood = () => {
 /* =========================
    일기 목록 출력
 ========================= */
-
-const renderDiaryList = () => {
+const renderDiaryList = (clickedPage = 1) => {
 
     const selectedEmotion = getSelectedEmotionFilter();
 
@@ -274,43 +268,68 @@ const renderDiaryList = () => {
 
 
             return matchEmotion && matchSearch;
-        });
+        })
+            .slice()
+            .reverse();
+
+
+    /* 마지막 페이지 계산 */
+    const lastPage = Math.ceil(visibleList.length / DIARY_COUNT_PER_PAGE);
+
+
+    /*
+     * 삭제해서 현재 페이지가 없어졌을 경우
+     * 마지막으로 존재하는 페이지로 이동
+     */
+    if (
+        lastPage > 0 &&
+        clickedPage > lastPage
+    ) {
+
+        clickedPage = lastPage;
+
+        startPage = Math.floor((clickedPage - 1) / PAGE_COUNT_PER_GROUP) * PAGE_COUNT_PER_GROUP + 1;
+    }
+
+
+    currentPage = clickedPage;
+
 
     /* 조건에 맞는 일기가 없을 경우 */
     if (visibleList.length === 0) {
 
-        diaryListArea.innerHTML = `
-            <p class="diary-default-text">
-                등록된 일기가 없습니다.
-            </p>
-        `;
+        diaryListArea.innerHTML = `<p class="diary-default-text">등록된 일기가 없습니다.</p>`;
+
+        startPage = 1;
+        currentPage = 1;
+
+        renderPagination(0, currentPage);
 
         return;
     }
 
 
-    /* 최신 일기부터 출력 */
+    /* 현재 페이지에 보여줄 일기만 가져오기 */
+    const currentDiaryList =
+        visibleList.filter((diary, index) => {
+
+            const skipCount = (clickedPage - 1) * DIARY_COUNT_PER_PAGE;
+
+            const skipIndex = skipCount - 1;
+
+
+            return (skipIndex < index && index <= skipIndex + DIARY_COUNT_PER_PAGE
+            );
+        });
+
+
     diaryListArea.innerHTML =
-        visibleList
-            .slice()
-            .reverse()
-            .map((diary) => {
-
+        currentDiaryList.map((diary) => {
                 return `
-                    <article
-                        class="diary-card ${getMoodClass(diary.mood)}"
-                        data-id="${diary.id}"
-                    >
+                    <article class="diary-card ${getMoodClass(diary.mood)}"data-id="${diary.id}">
 
-                        <button
-                            type="button"
-                            class="delete-button"
-                            data-id="${diary.id}"
-                            aria-label="일기 삭제"
-                        >
-                            <img
-                                src="./img/close_outline_light_s.svg"
-                            />
+                        <button type="button" class="delete-button" data-id="${diary.id}" aria-label="일기 삭제">
+                            <img src="./img/close_outline_light_s.svg"/>
                         </button>
 
 
@@ -353,8 +372,167 @@ const renderDiaryList = () => {
                 `;
             })
             .join("");
+
+
+    /* 목록과 페이지 버튼 함께 출력 */
+    renderPagination(lastPage, clickedPage);
 };
 
+/* =========================
+   페이지네이션
+========================= */
+
+
+/* 페이지 버튼 그리기 */
+const renderPagination = (lastPage, clickedPage
+) => {
+
+    /* 일기가 없을 경우 페이지네이션 숨기기 */
+    if (lastPage === 0) {
+
+        pagination.style.display = "none";
+
+        paginationNumberArea.innerHTML = "";
+
+        return;
+    }
+
+
+    pagination.style.display = "flex";
+
+
+    /* 길이가 10인 배열 */
+    const pageBox =
+        new Array(PAGE_COUNT_PER_GROUP).fill("page");
+
+
+    const pages = pageBox.map((el, index) => {
+
+            const pageNumber =
+                startPage + index;
+
+            return pageNumber <= lastPage
+                ? `
+                    <button
+                        type="button"
+                        class="pagination-number ${clickedPage === pageNumber ? "active" : ""}"
+                        onclick="renderDiaryList(${pageNumber});"
+                        >
+                        ${pageNumber}
+                    </button>
+                `
+                : "";
+
+        }).join("");
+
+
+    paginationNumberArea.innerHTML = pages;
+
+
+    /* 첫 번째 페이지 묶음인지 확인 */
+    const isFirstGroup =
+        startPage === 1;
+
+
+    /* 다음 페이지 묶음이 있는지 확인 */
+    const hasNextGroup = startPage + PAGE_COUNT_PER_GROUP <= lastPage;
+
+
+    paginationPrevButton.disabled = isFirstGroup;
+
+    paginationNextButton.disabled = !hasNextGroup;
+
+    /* 화살표 숨기기 */
+    if (lastPage === 1) {
+        paginationPrevButton.style.display = "none";
+        paginationNextButton.style.display = "none";
+    } else {
+        paginationPrevButton.style.display = "flex";
+        paginationNextButton.style.display = "flex";
+    }
+
+    /* 이전 버튼 이미지 변경 */
+    paginationPrevImage.src = isFirstGroup ? "./img/rightdisabled_outline_light_m.svg" : "./img/rightenable_outline_light_m.svg";
+
+
+    /* 다음 버튼 이미지 변경 */
+    paginationNextImage.src = hasNextGroup ? "./img/rightenable_outline_light_m.svg" : "./img/rightdisabled_outline_light_m.svg";
+};
+
+
+/* 이전 페이지 묶음으로 이동 */
+const movePrevPageGroup = () => {
+
+    if (startPage === 1) {
+        return;
+    }
+
+
+    startPage = startPage - PAGE_COUNT_PER_GROUP;
+
+    currentPage = startPage;
+
+
+    renderDiaryList(currentPage);
+};
+
+
+/* 다음 페이지 묶음으로 이동 */
+const moveNextPageGroup = () => {
+
+    const selectedEmotion =
+        getSelectedEmotionFilter();
+
+    const keyword = searchInput.value
+        .trim()
+        .toLowerCase();
+
+
+    /*
+     * 현재 검색과 감정 조건을 만족하는
+     * 일기만 가져오기
+     */
+    const visibleList = diaryListData.filter((diary) => {
+
+        const matchEmotion =
+            selectedEmotion === "all" ||
+            getMoodClass(diary.mood) === selectedEmotion;
+
+
+        const searchText = `${diary.title} ${diary.content}`
+                .toLowerCase();
+
+
+        const matchSearch = searchText.includes(keyword);
+
+
+        return matchEmotion && matchSearch;
+        });
+
+
+    const lastPage = Math.ceil(
+        visibleList.length / DIARY_COUNT_PER_PAGE
+    );
+
+
+    if (
+        startPage + PAGE_COUNT_PER_GROUP <= lastPage
+    ) {
+
+        startPage = startPage + PAGE_COUNT_PER_GROUP;
+
+        currentPage = startPage;
+
+
+        renderDiaryList(currentPage);
+    }
+};
+/* 이전 페이지 묶음 버튼 */
+paginationPrevButton.addEventListener("click", movePrevPageGroup);
+
+
+/* 다음 페이지 묶음 버튼 */
+paginationNextButton.addEventListener("click", moveNextPageGroup);
 
 /* =========================
    일기 등록
@@ -426,9 +604,11 @@ const handleSubmit = (event) => {
      */
     saveData();
 
+    startPage = 1;
+    currentPage = 1;
 
     /* 목록 다시 출력 */
-    renderDiaryList();
+    renderDiaryList(currentPage);
 
 
     /* 입력폼 초기화 */
@@ -436,9 +616,8 @@ const handleSubmit = (event) => {
 
 
     /* 감정을 행복으로 초기화 */
-    const firstMood =
-        document.querySelector(
-            'input[name="mood"][value="행복"]'
+    const firstMood = document.querySelector(
+        'input[name="mood"][value="행복"]'
         );
 
 
@@ -484,26 +663,35 @@ emotionFilterInputs.forEach((input) => {
                 .querySelector("span")
                 .textContent;
 
-        selectedEmotionText.textContent =
-            selectedLabel;
+        selectedEmotionText.textContent = selectedLabel;
 
         /* 드롭다운 닫기 */
         emotionDropdownToggle.checked = false;
 
+        /* 감정 필터 변경 시 1페이지로 이동 */
+        startPage = 1;
+        currentPage = 1;
+
+
         /* 선택한 감정으로 일기 다시 출력 */
-        renderDiaryList();
+        renderDiaryList(currentPage);
     });
 
 });
 
 /* 검색어 입력 */
-searchInput.addEventListener(
-    "input",
-    () => {
+searchInput.addEventListener("input", () => {
         clearTimeout(searchTimer);
 
         searchTimer = setTimeout(() => {
-            renderDiaryList();
+
+            /* 검색할 때 1페이지로 이동 */
+            startPage = 1;
+            currentPage = 1;
+
+
+            renderDiaryList(currentPage);
+
         }, 700);
     }
 );
@@ -513,19 +701,14 @@ searchInput.addEventListener(
    일기 등록 Form
 ========================= */
 
-diaryForm.addEventListener(
-    "submit",
-    handleSubmit
-);
+diaryForm.addEventListener("submit", handleSubmit);
 
 
 /* =========================
    화면 상단 이동
 ========================= */
 
-scrollTopButton.addEventListener(
-    "click",
-    () => {
+scrollTopButton.addEventListener("click", () => {
 
         window.scrollTo({
             top: 0,
@@ -549,8 +732,13 @@ window.addEventListener("load", () => {
     loadData();
 
 
+    /* 첫 화면은 1페이지부터 출력 */
+    startPage = 1;
+    currentPage = 1;
+
+
     /* 일기 목록 출력 */
-    renderDiaryList();
+    renderDiaryList(currentPage);
 });
 
 
@@ -590,18 +778,14 @@ function closeSuccessModal() {
 
 
 /* 일기쓰기 버튼 클릭 */
-openDiaryModalButton.addEventListener(
-    "click",
-    () => {
+openDiaryModalButton.addEventListener("click", () => {
         openDiaryModal();
     }
 );
 
 
 /* 등록 완료 확인 버튼 클릭 */
-closeSubmitDiaryButton.addEventListener(
-    "click",
-    () => {
+closeSubmitDiaryButton.addEventListener("click", () => {
 
         closeSuccessModal();
 
@@ -612,9 +796,7 @@ closeSubmitDiaryButton.addEventListener(
 
 
 /* 일기 작성 모달 배경 클릭 시 닫기 */
-diaryModal.addEventListener(
-    "click",
-    (event) => {
+diaryModal.addEventListener("click", (event) => {
 
         if (event.target === diaryModal) {
             closeDiaryModal();
@@ -625,9 +807,7 @@ diaryModal.addEventListener(
 
 
 /* 등록 완료 모달 배경 클릭 시 닫기 */
-successDiaryModal.addEventListener(
-    "click",
-    (event) => {
+successDiaryModal.addEventListener("click", (event) => {
 
         if (event.target === successDiaryModal) {
 
@@ -642,9 +822,7 @@ successDiaryModal.addEventListener(
 
 
 /* ESC 키로 모달 닫기 */
-document.addEventListener(
-    "keydown",
-    (event) => {
+document.addEventListener("keydown", (event) => {
 
         if (event.key === "Escape") {
 
@@ -666,50 +844,43 @@ document.addEventListener(
 /* 상세페이지 이동 */
 function detail(id) {
 
-    location.href =
-        "./diary-detail.html?id=" + id;
+    location.href = "./diary-detail.html?id=" + id;
 }
 
 
 diaryListArea.addEventListener("click", (event) => {
 
     /* 삭제 버튼 클릭 */
-    const deleteButton =
-        event.target.closest(".delete-button");
+    const deleteButton = event.target.closest(".delete-button");
 
     if (deleteButton !== null) {
 
-        const diaryId =
-            Number(deleteButton.dataset.id);
+        const diaryId = Number(deleteButton.dataset.id);
 
-        diaryListData =
-            diaryListData.filter((diary) => {
-                return diary.id !== diaryId;
+        diaryListData = diaryListData.filter((diary) => {
+            return diary.id !== diaryId;
             });
 
-        commentListData =
-            commentListData.filter((comment) => {
-                return comment.diaryId !== diaryId;
+        commentListData = commentListData.filter((comment) => {
+            return comment.diaryId !== diaryId;
             });
 
         saveData();
 
-        renderDiaryList();
+        renderDiaryList(currentPage);
 
         return;
     }
 
 
     /* 일기 카드 클릭 */
-    const diaryCard =
-        event.target.closest(".diary-card");
+    const diaryCard = event.target.closest(".diary-card");
 
     if (diaryCard === null) {
         return;
     }
 
-    const diaryId =
-        diaryCard.dataset.id;
+    const diaryId = diaryCard.dataset.id;
 
     detail(diaryId);
 });
